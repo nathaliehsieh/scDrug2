af8f6008ac4ab9f44c49a8f588310612c79232d3
blackjack061524 <blackjack061524@gmail.com>
2021-07-28 11:04:18 +0800

    update script for command line input

--- script/single_cell_analysis.py
+++ script/single_cell_analysis.py
@@ -1,32 +1,91 @@
+import argparse, os, sys
 import numpy as np
 import pandas as pd
 import scanpy as sc
 
-results_file = '../write/GSE156625_HCCscanpyobj.h5ad'
+## Parse command-line arguments
+# process arguments
+parser = argparse.ArgumentParser(description="scRNA-seq data analysis")
 
-adata = sc.read_10x_mtx(
-        '../data/GSE156625_HCC/',
-        var_names='gene_symbols',
-        cache=True)
-adata.var_names_make_unique()
+parser.add_argument("-i", "--input", required=True, help="path to input 10x directory or CSV file")
+parser.add_argument("-f", "--format", default='10x', help="input format, 10x (default) | csv | h5ad (Anndata object for subclustering with --clusters CLUSTERS)")
+parser.add_argument("-o", "--output", default='./', help="path to output directory, default='./'")
+parser.add_argument("-r", "--resolution", type=float, default=0.6, help="resolution for clustering, default=0.6")
+# parser.add_argument("--auto-resolution", type=bool, default=False, help="set True to automatically determine resolution for clustering, default=False")
+parser.add_argument("-m", "--metadata", default=None, help="path to metadata CSV file for batch correction (index as input in first column)")
+parser.add_argument("-b", "--batch", default=None, help="column in metadata for batch correction, e.g. 'patient_id'")
+parser.add_argument("-c", "--clusters", default=None, help="perform single cell analysis only on specified clusters, e.g. '1,3,8,9'")
+
+args = parser.parse_args()
+
+# check format, input and clusters
+if not os.path.exists(args.input):
+    sys.exit("The input path does not exist.")
+if args.format == 'csv':
+    if args.input[-4:] != '.csv':
+        sys.exit("The input file is not a CSV file.")
+elif args.format == '10x':
+    if not os.path.exists(os.path.join(args.input, 'matrix.mtx')):
+        sys.exit("Cannot find 'matrix.mtx' file in the input directory.")
+    if not os.path.exists(os.path.join(args.input, 'genes.tsv')):
+        if not os.path.exists(os.path.join(args.input, 'features.tsv')):
+            sys.exit("Cannot find 'genes.tsv' or 'features.tsv' file in the input directory.")
+    if not os.path.exists(os.path.join(args.input, 'barcodes.tsv')):
+        sys.exit("Cannot find 'barcodes.tsv' file in the input directory.")
+elif args.format == 'h5ad':
+    if args.input[-5:] != '.h5ad':
+        sys.exit("The input file is not a h5ad file.")
+    if args.clusters is None:
+        sys.exit("Need to speficy clusters to be analyzed with a h5ad file.")
+else:
+     sys.exit("The format can only be '10x' or 'csv'.")
+
+# check output
+if not os.path.isdir(args.output):
+    sys.exit("The output directory does not exist.")
+
+# check metadata
+if not args.metadata is None:
+    if not os.path.exists(args.metadata):
+        sys.exit("The metadata file does not exist.")
+    if args.metadata[-4:] != '.csv':
+        sys.exit("The metadata file is not a CSV file.")
+
+# check batch
+if not args.batch is None:
+    metadata_df = pd.read_csv(args.metadata, index_col=0)
+    if not args.batch in metadata_df.columns:
+        sys.exit("The batch column is not in the metadata file.")
 
 
 ## Preprocessing
-sc.pp.filter_cells(adata, min_genes=200)
-sc.pp.filter_genes(adata, min_cells=3)
+results_file = os.path.join(args.output, 'scanpyobj.h5ad')
 
-adata.var['mt'] = adata.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+if args.format == 'h5ad':
+    adata = sc.read(args.input)
+    clusters = [x.strip() for x in args.clusters.split(',')]
+    adata = adata.raw.to_adata()[adata.obs['louvain'].isin(clusters)]
+else:
+    if args.format == 'csv':
+        adata = sc.read_csv(args.input)
+    elif args.format == '10x':
+        adata = sc.read_10x_mtx(args.input, var_names='gene_symbols', cache=True)
+    adata.var_names_make_unique()
 
-adata = adata[adata.obs.pct_counts_mt < 30, :]
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=3)
+    
+    adata.var['mt'] = adata.var_names.str.startswith('MT-')
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    
+    adata = adata[adata.obs.pct_counts_mt < 30, :]
+    
+    adata.raw = adata
 
 sc.pp.normalize_total(adata, target_sum=1e4)
 sc.pp.log1p(adata)
 
 sc.pp.highly_variable_genes(adata)
-# sc.pl.highly_variable_genes(adata)
-
-adata.raw = adata
 adata = adata[:, adata.var.highly_variable]
 sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
 
@@ -35,148 +94,118 @@ sc.pp.scale(adata)
 
 ## Principal component analysis
 sc.tl.pca(adata, svd_solver='arpack')
-# sc.pl.pca_variance_ratio(adata, log=True)
 
 adata.write(results_file)
 
 
 ## Batch Correction with Harmony
-section_df = pd.read_csv('../data/section_info.csv')
-patient_id = [int(barcode.split('-')[-1]) for barcode in adata.obs.index]
-adata.obs['PatientID'] = [section_df['PatientID'][pid-1] for pid in patient_id]
-adata.obs['NormalvsTumor'] = [section_df['NormalvsTumor'][pid-1] for pid in patient_id]
-sc.external.pp.harmony_integrate(adata, 'PatientID', adjusted_basis='X_pca')
+if not args.batch is None:
+    adata.obs[args.batch] = metadata_df.loc[adata.obs.index][args.batch]
+    sc.external.pp.harmony_integrate(adata, args.batch, adjusted_basis='X_pca')
 
-## Computing the neighborhood graph
+
+## Clustering
 sc.pp.neighbors(adata, n_pcs=20)
-
-## Embedding the neighborhood graph
 sc.tl.umap(adata)
-
-## Clustering the neighborhood graph
-sc.tl.louvain(adata, resolution=0.6)
-# sc.pl.umap(adata, color=['louvain'], use_raw=False)
-# sc.pl.umap(adata, color=['PatientID'], use_raw=False)
+sc.tl.louvain(adata, resolution=args.resolution)
 
 adata.write(results_file)
 
-## Finding differentially expressed genes
-method = "t-test"
-sc.tl.rank_genes_groups(adata, 'louvain', method=method)
-# sc.pl.rank_genes_groups(adata, n_genes=25, sharey=False)
 
-
-## scMatch
-# Export csv used by scMatch
-result = adata.uns['rank_genes_groups']
-groups = result['names'].dtype.names
-mat = np.zeros((len(adata.raw.var.index), len(groups)), dtype=float)
-for group in groups:
-    mat[: , int(group)] = adata.raw.X[adata.obs['louvain']==group].mean(axis=0)
-dat = pd.DataFrame(mat, index = adata.raw.var.index, columns = groups)
-dat.to_csv("../data/harmony_scanpy_cluster_" + method + "_GSE156625_HCC_scMatch.csv")
-
-# sh scMatch_cluster.sh
-
-# Cell annotation result
-scMatch_cluster_df = pd.read_csv('../data/harmony_scanpy_cluster_' + method + '_GSE156625_HCC_scMatch/annotation_result_keep_all_genes/human_Spearman_top_ann.csv')
-scMatch_cluster_names = [group + "_" + scMatch_cluster_df.loc[scMatch_cluster_df['cell']==int(group)]\
-                          ['cell type'].tolist()[0] for group in groups]
-# adata.rename_categories('louvain', scMatch_cluster_names)
-# sc.pl.umap(adata, color=['louvain'], legend_loc='on data', use_raw=False)
-
-
-## Specify Tumor cluster
-total_tumor_n = sum(adata.obs['NormalvsTumor'])
-total_normal_n = len(adata.obs) - total_tumor_n
-cluster_tumor_percentage = np.zeros(len(groups), dtype = float)
-cluster_normal_percentage = np.zeros(len(groups), dtype = float)
-for group in groups:
-    cells = adata.obs['louvain'] == group
-    cluster_tumor_n = sum(adata.obs['NormalvsTumor'][cells])
-    cluster_tumor_percentage[int(group)] = cluster_tumor_n / total_tumor_n
-    cluster_normal_percentage[int(group)] = (sum(cells) - cluster_tumor_n) / total_normal_n
-
-# import matplotlib.pyplot as plt
-# plt.scatter(cluster_normal_percentage, cluster_tumor_percentage) 
-# plt.show()
-
-tumor_cluster = np.where(cluster_tumor_percentage/cluster_normal_percentage > 2)[0].astype(str)
-tumor_adata = adata.raw.to_adata()[adata.obs['louvain'].isin(tumor_cluster)]
-
-sc.pp.highly_variable_genes(tumor_adata)
-tumor_adata = tumor_adata[:, tumor_adata.var.highly_variable]
-sc.pp.regress_out(tumor_adata, ['total_counts', 'pct_counts_mt'])
-
-sc.pp.scale(tumor_adata)
-
-# Principal component analysis
-sc.tl.pca(tumor_adata, svd_solver='arpack')
-# sc.pl.pca_variance_ratio(tumor_adata, log=True)
-
-# Batch Correction with Harmony
-section_df = pd.read_csv('../data/section_info.csv')
-patient_id = [int(barcode.split('-')[-1]) for barcode in tumor_adata.obs.index]
-tumor_adata.obs['PatientID'] = [section_df['PatientID'][pid-1] for pid in patient_id]
-tumor_adata.obs['NormalvsTumor'] = [section_df['NormalvsTumor'][pid-1] for pid in patient_id]
-# sc.external.pp.harmony_integrate(tumor_adata, 'PatientID')
-sc.external.pp.harmony_integrate(tumor_adata, 'PatientID', adjusted_basis='X_pca')
-
-# Computing the neighborhood graph
-sc.pp.neighbors(tumor_adata, n_pcs=20)
-
-# Embedding the neighborhood graph
-sc.tl.umap(tumor_adata)
-
-
-## SCCAF
-from SCCAF import *
-
-sc.tl.louvain(tumor_adata, resolution=2, key_added = 'L2_Round0')
-sc.pl.umap(tumor_adata, color=['L2_Round0'], use_raw=False)
-
-# SCCAF recover
-SCCAF_optimize_all(ad=tumor_adata, plot=False, min_acc=0.9,  prefix = 'L2', use='pca')
-sc.pl.umap(tumor_adata, color=['L2_result'], use_raw=False)
-
-# Calculate silhouette score
-from sklearn.metrics import silhouette_samples, silhouette_score
-import matplotlib.pyplot as plt
-
-X = tumor_adata.obsm['X_pca'][:, :20]
-SCCAF_silhouette_avg = silhouette_score(X, tumor_adata.obs['L2_result'])
-# SCCAF_sample_silhouette_values = silhouette_samples(tumor_adata.obsm['X_pca'], tumor_adata.obs['L2_result'])
-# SCCAF_cluster_silhouette_values = [np.mean(SCCAF_sample_silhouette_values[np.where(tumor_adata.obs['louvain']==group)])
-#                                    for group in tumor_adata.obs['louvain'].unique()]
-
-## Louvain with silhouette score
-R = np.arange(0.1, 1.6, 0.1)
-silhouette_avg = []
-sample_silhouette_values = []
-for res in R: 
-    sc.tl.louvain(tumor_adata, resolution=res)
-    # sc.pl.umap(tumor_adata, color=['louvain'], title='louvain, res=' + str(np.round(res, 1)))
+groups = np.sort(adata.obs['louvain'].unique())
+if args.clusters is None:
+    ## scMatch
+    # Export csv used by scMatch
+    mat = np.zeros((len(adata.raw.var.index), len(groups)), dtype=float)
+    for group in groups:
+        mat[: , int(group)] = adata.raw.X[adata.obs['louvain']==group].mean(axis=0)
+    dat = pd.DataFrame(mat, index = adata.raw.var.index, columns = groups)
+    dat.to_csv(os.path.join(args.output, 'cluster_mean_exp.csv'))
     
-    ## Calculate silhouette score
-    silhouette_avg.append(silhouette_score(X, tumor_adata.obs['louvain']))
-    # sample_silhouette_values.append(silhouette_samples(X, tumor_adata.obs['louvain']))
-    # cluster_silhouette_values = [np.mean(sample_silhouette_values[0][np.where(tumor_adata.obs['louvain']==group)])
-    #                               for group in tumor_adata.obs['louvain'].unique()]
-best_res = R[np.argmax(silhouette_avg)]
+    os.system('python /opt/scMatch/scMatch.py --refDS /opt/scMatch/refDB/FANTOM5 \
+              --dFormat csv --testDS ' + os.path.join(args.output, 'cluster_mean_exp.csv'))
+    
+    # Cell annotation result
+    scMatch_cluster_df = pd.read_csv(os.path.join(args.output, 'cluster_mean_exp') + '/annotation_result_keep_all_genes/human_Spearman_top_ann.csv')
+    scMatch_cluster_names = [group + " " + scMatch_cluster_df.loc[scMatch_cluster_df['cell']==int(group)]\
+                              ['cell type'].tolist()[0] for group in groups]
+    adata.rename_categories('louvain', scMatch_cluster_names)
 
+sc.settings.autosave = True
+sc.settings.figdir = args.output
+sc.pl.umap(adata, color=['louvain'], use_raw=False, show=False, save='_cluster.png')
+if not args.batch is None:
+    sc.pl.umap(adata, color=[args.batch], use_raw=False, show=False, save='_batch.png')
 
-## TODO: Determine sub-clustering result
 
 ## Finding differentially expressed genes
+adata.rename_categories('louvain', groups)
 method = "t-test"
-sc.tl.rank_genes_groups(tumor_adata, 'louvain', method=method)
-# sc.pl.rank_genes_groups(tumor_adata, n_genes=25, sharey=False)
+sc.tl.rank_genes_groups(adata, 'louvain', use_raw=False, method=method)
+
+adata.write(results_file)
+
+# cluster DEGs
+result = adata.uns['rank_genes_groups']
+dat = pd.DataFrame({group + '_' + key: result[key][group] for group in groups for key in ['names', 'logfoldchanges','scores','pvals']})
+dat.to_csv(os.path.join(args.output, 'cluster_DEGs.csv'))
+
+# GEP format
+adata_GEP = adata.raw.to_adata()
+sc.pp.normalize_total(adata, target_sum=1e6)
+mat = adata_GEP.X.transpose()
+dat = pd.DataFrame(mat.toarray(), index=adata_GEP.var.index, columns=adata.obs['louvain'])
+dat.to_csv(os.path.join(args.output, 'GEP.csv'))
 
 
-## CaDRRes-SC
-result = tumor_adata.uns['rank_genes_groups']
-cluster = '0'
-dat = pd.DataFrame({'cluster' + cluster + '_log_fc' : result['logfoldchanges'][cluster]}, index = result['names'][cluster])
-dat.to_csv("../data/GSE156625_HCC_tumor_cluster' + cluster + '_log_fc.csv")
+# ## SCCAF
+# from SCCAF import *
 
-# python3 CaDRRes-SC_predicting_cluster_drug_response.py
+# sc.tl.louvain(tumor_adata, resolution=2, key_added = 'L2_Round0')
+# sc.pl.umap(tumor_adata, color=['L2_Round0'], use_raw=False)
+
+# # SCCAF recover
+# SCCAF_optimize_all(ad=tumor_adata, plot=False, min_acc=0.9,  prefix = 'L2', use='pca')
+# sc.pl.umap(tumor_adata, color=['L2_result'], use_raw=False)
+
+# # Calculate silhouette score
+# from sklearn.metrics import silhouette_samples, silhouette_score
+# import matplotlib.pyplot as plt
+
+# X = tumor_adata.obsm['X_pca'][:, :20]
+# SCCAF_silhouette_avg = silhouette_score(X, tumor_adata.obs['L2_result'])
+# # SCCAF_sample_silhouette_values = silhouette_samples(tumor_adata.obsm['X_pca'], tumor_adata.obs['L2_result'])
+# # SCCAF_cluster_silhouette_values = [np.mean(SCCAF_sample_silhouette_values[np.where(tumor_adata.obs['louvain']==group)])
+# #                                    for group in tumor_adata.obs['louvain'].unique()]
+
+# ## Louvain with silhouette score
+# R = np.arange(0.1, 1.6, 0.1)
+# silhouette_avg = []
+# sample_silhouette_values = []
+# for res in R: 
+#     sc.tl.louvain(tumor_adata, resolution=res)
+#     # sc.pl.umap(tumor_adata, color=['louvain'], title='louvain, res=' + str(np.round(res, 1)))
+    
+#     ## Calculate silhouette score
+#     silhouette_avg.append(silhouette_score(X, tumor_adata.obs['louvain']))
+#     # sample_silhouette_values.append(silhouette_samples(X, tumor_adata.obs['louvain']))
+#     # cluster_silhouette_values = [np.mean(sample_silhouette_values[0][np.where(tumor_adata.obs['louvain']==group)])
+#     #                               for group in tumor_adata.obs['louvain'].unique()]
+# best_res = R[np.argmax(silhouette_avg)]
+
+
+# ## TODO: Determine sub-clustering result
+
+# ## Finding differentially expressed genes
+# method = "t-test"
+# sc.tl.rank_genes_groups(tumor_adata, 'louvain', method=method)
+# # sc.pl.rank_genes_groups(tumor_adata, n_genes=25, sharey=False)
+
+
+# ## CaDRRes-SC
+# result = tumor_adata.uns['rank_genes_groups']
+# cluster = '0'
+# dat = pd.DataFrame({'cluster' + cluster + '_log_fc' : result['logfoldchanges'][cluster]}, index = result['names'][cluster])
+# dat.to_csv("../data/GSE156625_HCC_tumor_cluster' + cluster + '_log_fc.csv")
+
+# # python3 CaDRRes-SC_predicting_cluster_drug_response.py
