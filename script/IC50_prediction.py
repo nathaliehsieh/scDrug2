1c13938e8421f17bd27ebeea0aea174bb552af48
blackjack061524 <blackjack061524@gmail.com>
2021-07-29 15:05:14 +0800

    update scripts for GSEA, IC50 prediction

---
+++ script/IC50_prediction.py
@@ -0,0 +1,80 @@
+## Prediction based on CaDRReS-Sc pre-trained model
+import argparse, sys, os, pickle
+from collections import Counter
+import importlib
+from ipywidgets import widgets
+import warnings
+warnings.filterwarnings('ignore')
+import pandas as pd
+import numpy as np
+import scanpy as sc
+
+
+## Parse command-line arguments
+# process arguments
+parser = argparse.ArgumentParser(description="IC50 prediction")
+
+parser.add_argument("-i", "--input", required=True, help="path to input Anndata object (h5ad file)")
+parser.add_argument("-o", "--output", default='./', help="path to output directory, default='./'")
+parser.add_argument("-c", "--clusters", default='All', type=str, help="perform IC50 prediction on specified clusters, e.g. '1,3,8,9', default='All'")
+
+args = parser.parse_args()
+
+# check input
+if not os.path.exists(args.input):
+    sys.exit("The input path does not exist.")
+if args.input[-5:] != '.h5ad':
+    sys.exit("The input file is not a h5ad file.")
+
+# check output
+if not os.path.isdir(args.output):
+    sys.exit("The output directory does not exist.")
+
+
+scriptpath = '/opt/CaDRReS-Sc'
+sys.path.append(os.path.abspath(scriptpath))
+
+from cadrres_sc import pp, model, evaluation, utility
+
+## Read pre-trained model
+model_dir = '/single-cell-analysis/CaDRReS-Sc-model/'
+obj_function = widgets.Dropdown(options=['cadrres-wo-sample-bias', 'cadrres-wo-sample-bias-weight'], description='Objetice function')
+model_spec_name = obj_function.value
+model_file = model_dir + '{}_param_dict_all_genes.pickle'.format(model_spec_name)
+cadrres_model = model.load_model(model_file)
+
+## Read test data
+gene_exp_df = pd.read_csv(scriptpath + '/data/GDSC/GDSC_exp.tsv', sep='\t', index_col=0)
+gene_exp_df = gene_exp_df.groupby(gene_exp_df.index).mean()
+
+## Load cluster-specific gene expression profile
+adata = sc.read(args.input)
+if args.clusters != 'All':
+    clusters = [x.strip() for x in args.clusters.split(',')]
+    adata = adata[adata.obs['louvain'].isin(clusters)]
+cluster_exp_df = pd.DataFrame(adata.raw.X.transpose().toarray(), index=adata.raw.var.index, columns=adata.obs.index)
+
+## Calculate fold-change
+cell_line_log2_mean_fc_exp_df, cell_line_mean_exp_df = pp.gexp.normalize_log2_mean_fc(gene_exp_df)
+cluster_log2_mean_fc_exp_df, cluster_mean_exp_df = pp.gexp.normalize_log2_mean_fc(cluster_exp_df)
+# cluster_log2_mean_fc_exp_df
+
+## Read essential genes list
+ess_gene_list = gene_exp_df.index.dropna().tolist()
+
+## Calculate kernel feature
+test_kernel_df = pp.gexp.calculate_kernel_feature(cluster_log2_mean_fc_exp_df, cell_line_log2_mean_fc_exp_df, ess_gene_list)
+
+## Drug response prediction
+print('Predicting drug response using CaDRReS: {}'.format(model_spec_name))
+pred_df, P_test_df= model.predict_from_model(cadrres_model, test_kernel_df, model_spec_name)
+print('done!')
+
+pred_mean_df = pred_df.mean()
+pred_mean_df = pred_mean_df.groupby(pred_mean_df.index).mean()
+pred_mean_df = pred_mean_df.sort_values()
+drug_df = pd.read_csv(scriptpath + '/preprocessed_data/GDSC/drug_stat.csv', index_col=0)
+pred_mean_df.index = [drug_df.loc[int(drug_id)]['Drug Name'] for drug_id in pred_mean_df.index]
+pred_mean_df.columns = [args.clusters]
+pred_mean_df.to_csv(os.path.join(args.output, 'IC50_prediction.csv'))
+
--- script/single_cell_analysis.py
+++ script/single_cell_analysis.py
@@ -3,6 +3,48 @@ import numpy as np
 import pandas as pd
 import scanpy as sc
 
+def runGSEAPY(adata, group_by='louvain', gene_sets=['GO_Biological_Process_2021'], organism='Human', cutoff=0.05, logfc_threshold=2):
+    import gseapy as gp
+
+    df_list = []
+    cluster_list = []
+    for celltype in set(adata.obs[group_by]):
+        indlist_logfc = adata.uns['rank_genes_groups']['logfoldchanges'][celltype] >= logfc_threshold
+        indlist_adjp = adata.uns['rank_genes_groups']['pvals_adj'][celltype] <= 1e-2
+        indlist_p = adata.uns['rank_genes_groups']['pvals'][celltype] <= 1e-2
+        #indlist_pts = adata.uns['rank_genes_groups']['pts'][celltype] >= 0.1
+        
+        indlist = indlist_logfc * indlist_adjp * indlist_p 
+
+        ind = [x for x in range(0, len(indlist)) if indlist[x] ]
+        degs = adata.uns['rank_genes_groups']['names'][celltype][ind].tolist()
+        
+        if not degs:
+            continue
+
+        enr = gp.enrichr(gene_list=degs,
+                gene_sets=gene_sets,
+                organism=organism, 
+                description=celltype,
+                no_plot=True
+                )
+        df_list.append(enr.res2d)
+        cluster_list.append(celltype)
+    
+    columns = ['Cluster', 'Gene_set', 'Term', 'Overlap', 'P-value', 'Adjusted P-value', 'Genes']
+
+    df = pd.DataFrame(columns = columns)
+    for cluster_ind, df_ in zip(cluster_list, df_list):
+        df_ = df_[df_['Adjusted P-value'] <= cutoff]
+        df_ = df_.assign(Cluster = cluster_ind)
+        if(df_.shape[0] > 0):
+            df = pd.concat([df, df_[columns]], sort=False)
+        else:
+            print('No pathway with an adjusted P-value less than the cutoff (={}) for cluster {}'.format(cutoff, cluster_ind))
+    
+    return df
+
+
 ## Parse command-line arguments
 # process arguments
 parser = argparse.ArgumentParser(description="scRNA-seq data analysis")
@@ -11,10 +53,12 @@ parser.add_argument("-i", "--input", required=True, help="path to input 10x dire
 parser.add_argument("-f", "--format", default='10x', help="input format, 10x (default) | csv | h5ad (Anndata object for subclustering with --clusters CLUSTERS)")
 parser.add_argument("-o", "--output", default='./', help="path to output directory, default='./'")
 parser.add_argument("-r", "--resolution", type=float, default=0.6, help="resolution for clustering, default=0.6")
-# parser.add_argument("--auto-resolution", type=bool, default=False, help="set True to automatically determine resolution for clustering, default=False")
+# parser.add_argument("--auto-resolution", action="store_true", help="set True to automatically determine resolution for clustering, default=False")
 parser.add_argument("-m", "--metadata", default=None, help="path to metadata CSV file for batch correction (index as input in first column)")
 parser.add_argument("-b", "--batch", default=None, help="column in metadata for batch correction, e.g. 'patient_id'")
 parser.add_argument("-c", "--clusters", default=None, help="perform single cell analysis only on specified clusters, e.g. '1,3,8,9'")
+parser.add_argument("-a", "--annotation", action="store_true", help="perform cell type annotation")
+parser.add_argument("-g", "--gsea", action="store_true", help="perform gene set enrichment analsis (GSEA)")
 
 args = parser.parse_args()
 
@@ -25,12 +69,12 @@ if args.format == 'csv':
     if args.input[-4:] != '.csv':
         sys.exit("The input file is not a CSV file.")
 elif args.format == '10x':
-    if not os.path.exists(os.path.join(args.input, 'matrix.mtx')):
+    if not os.path.exists(os.path.join(args.input, 'matrix.mtx')) and not os.path.exists(os.path.join(args.input, 'matrix.mtx.gz')):
         sys.exit("Cannot find 'matrix.mtx' file in the input directory.")
-    if not os.path.exists(os.path.join(args.input, 'genes.tsv')):
-        if not os.path.exists(os.path.join(args.input, 'features.tsv')):
+    if not os.path.exists(os.path.join(args.input, 'genes.tsv')) and not os.path.exists(os.path.join(args.input, 'genes.tsv.gz')):
+        if not os.path.exists(os.path.join(args.input, 'features.tsv')) and not os.path.exists(os.path.join(args.input, 'features.tsv.gz')):
             sys.exit("Cannot find 'genes.tsv' or 'features.tsv' file in the input directory.")
-    if not os.path.exists(os.path.join(args.input, 'barcodes.tsv')):
+    if not os.path.exists(os.path.join(args.input, 'barcodes.tsv')) and not os.path.exists(os.path.join(args.input, 'barcodes.tsv.gz')):
         sys.exit("Cannot find 'barcodes.tsv' file in the input directory.")
 elif args.format == 'h5ad':
     if args.input[-5:] != '.h5ad':
@@ -79,13 +123,18 @@ else:
     sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
     
     adata = adata[adata.obs.pct_counts_mt < 30, :]
-    
-    adata.raw = adata
+
+# GEP preperation
+adata_GEP = adata.copy()
+sc.pp.normalize_total(adata_GEP, target_sum=1e6)
+mat = adata_GEP.X.transpose()
+GEP_df = pd.DataFrame(mat.toarray(), index=adata_GEP.var.index)
 
 sc.pp.normalize_total(adata, target_sum=1e4)
 sc.pp.log1p(adata)
 
 sc.pp.highly_variable_genes(adata)
+adata.raw = adata
 adata = adata[:, adata.var.highly_variable]
 sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
 
@@ -95,7 +144,7 @@ sc.pp.scale(adata)
 ## Principal component analysis
 sc.tl.pca(adata, svd_solver='arpack')
 
-adata.write(results_file)
+# adata.write(results_file)
 
 
 ## Batch Correction with Harmony
@@ -109,11 +158,11 @@ sc.pp.neighbors(adata, n_pcs=20)
 sc.tl.umap(adata)
 sc.tl.louvain(adata, resolution=args.resolution)
 
-adata.write(results_file)
+# adata.write(results_file)
 
 
 groups = np.sort(adata.obs['louvain'].unique())
-if args.clusters is None:
+if args.annotation:
     ## scMatch
     # Export csv used by scMatch
     mat = np.zeros((len(adata.raw.var.index), len(groups)), dtype=float)
@@ -129,19 +178,24 @@ if args.clusters is None:
     scMatch_cluster_df = pd.read_csv(os.path.join(args.output, 'cluster_mean_exp') + '/annotation_result_keep_all_genes/human_Spearman_top_ann.csv')
     scMatch_cluster_names = [group + " " + scMatch_cluster_df.loc[scMatch_cluster_df['cell']==int(group)]\
                               ['cell type'].tolist()[0] for group in groups]
-    adata.rename_categories('louvain', scMatch_cluster_names)
+    adata.obs['cell_type'] = adata.obs['louvain'].cat.rename_categories(scMatch_cluster_names)
 
 sc.settings.autosave = True
 sc.settings.figdir = args.output
 sc.pl.umap(adata, color=['louvain'], use_raw=False, show=False, save='_cluster.png')
 if not args.batch is None:
     sc.pl.umap(adata, color=[args.batch], use_raw=False, show=False, save='_batch.png')
+if args.annotation:
+    sc.pl.umap(adata, color=['cell_type'], use_raw=False, show=False, save='_cell_type.png')
+
+# GEP format
+GEP_df.columns = adata.obs['louvain'].tolist()
+GEP_df.to_csv(os.path.join(args.output, 'GEP.csv'))
 
 
 ## Finding differentially expressed genes
-adata.rename_categories('louvain', groups)
 method = "t-test"
-sc.tl.rank_genes_groups(adata, 'louvain', use_raw=False, method=method)
+sc.tl.rank_genes_groups(adata, 'louvain', method=method, pts=True)
 
 adata.write(results_file)
 
@@ -150,62 +204,9 @@ result = adata.uns['rank_genes_groups']
 dat = pd.DataFrame({group + '_' + key: result[key][group] for group in groups for key in ['names', 'logfoldchanges','scores','pvals']})
 dat.to_csv(os.path.join(args.output, 'cluster_DEGs.csv'))
 
-# GEP format
-adata_GEP = adata.raw.to_adata()
-sc.pp.normalize_total(adata, target_sum=1e6)
-mat = adata_GEP.X.transpose()
-dat = pd.DataFrame(mat.toarray(), index=adata_GEP.var.index, columns=adata.obs['louvain'])
-dat.to_csv(os.path.join(args.output, 'GEP.csv'))
+# perform GSEA
+if args.gsea:
+    df_gsea = runGSEAPY(adata)
+    df_gsea.to_csv(os.path.join(args.output, 'GSEA_results.csv'))
 
 
-# ## SCCAF
-# from SCCAF import *
-
-# sc.tl.louvain(tumor_adata, resolution=2, key_added = 'L2_Round0')
-# sc.pl.umap(tumor_adata, color=['L2_Round0'], use_raw=False)
-
-# # SCCAF recover
-# SCCAF_optimize_all(ad=tumor_adata, plot=False, min_acc=0.9,  prefix = 'L2', use='pca')
-# sc.pl.umap(tumor_adata, color=['L2_result'], use_raw=False)
-
-# # Calculate silhouette score
-# from sklearn.metrics import silhouette_samples, silhouette_score
-# import matplotlib.pyplot as plt
-
-# X = tumor_adata.obsm['X_pca'][:, :20]
-# SCCAF_silhouette_avg = silhouette_score(X, tumor_adata.obs['L2_result'])
-# # SCCAF_sample_silhouette_values = silhouette_samples(tumor_adata.obsm['X_pca'], tumor_adata.obs['L2_result'])
-# # SCCAF_cluster_silhouette_values = [np.mean(SCCAF_sample_silhouette_values[np.where(tumor_adata.obs['louvain']==group)])
-# #                                    for group in tumor_adata.obs['louvain'].unique()]
-
-# ## Louvain with silhouette score
-# R = np.arange(0.1, 1.6, 0.1)
-# silhouette_avg = []
-# sample_silhouette_values = []
-# for res in R: 
-#     sc.tl.louvain(tumor_adata, resolution=res)
-#     # sc.pl.umap(tumor_adata, color=['louvain'], title='louvain, res=' + str(np.round(res, 1)))
-    
-#     ## Calculate silhouette score
-#     silhouette_avg.append(silhouette_score(X, tumor_adata.obs['louvain']))
-#     # sample_silhouette_values.append(silhouette_samples(X, tumor_adata.obs['louvain']))
-#     # cluster_silhouette_values = [np.mean(sample_silhouette_values[0][np.where(tumor_adata.obs['louvain']==group)])
-#     #                               for group in tumor_adata.obs['louvain'].unique()]
-# best_res = R[np.argmax(silhouette_avg)]
-
-
-# ## TODO: Determine sub-clustering result
-
-# ## Finding differentially expressed genes
-# method = "t-test"
-# sc.tl.rank_genes_groups(tumor_adata, 'louvain', method=method)
-# # sc.pl.rank_genes_groups(tumor_adata, n_genes=25, sharey=False)
-
-
-# ## CaDRRes-SC
-# result = tumor_adata.uns['rank_genes_groups']
-# cluster = '0'
-# dat = pd.DataFrame({'cluster' + cluster + '_log_fc' : result['logfoldchanges'][cluster]}, index = result['names'][cluster])
-# dat.to_csv("../data/GSE156625_HCC_tumor_cluster' + cluster + '_log_fc.csv")
-
-# # python3 CaDRRes-SC_predicting_cluster_drug_response.py
