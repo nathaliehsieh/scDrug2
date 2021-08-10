1306c3e3f96784e4b26842a226cb74f20576a736
nathalie.hsieh <nathalie.hsieh@ailabs.tw>
2021-08-10 15:48:53 +0800

    added code for cell type determination when generating bulk GEPs

--- script/generate_L1000_GEP.py
+++ script/generate_L1000_GEP.py
@@ -1,7 +1,7 @@
-from cmapPy.pandasGEXpress.parse import parse
+# from cmapPy.pandasGEXpress.parse import parse
 import argparse
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 import os
 import sys
 
@@ -21,18 +21,20 @@ def downloadFromGEO(filename, url):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Generate bulk profiles of the specified cell type from the LINCS L1000 database.")
+    parser.add_argument("-o", "--outdir", default='./LINCS/', help="Path to output directory, default='./'")
+    parser.add_argument("-c", "--celltype", default=None, help='Cell Line name. Options:  A375 (malignant melanoma),  A549 (non-small cell lung carcinoma),  HCC515 (non-small cell lung adenocarcinoma),  HEPG2 (hepatocellular carcinoma), HT29 (colorectal adenocarcinoma),  MCF7 (breast adenocarcinoma),  PC3 (prostate adenocarcinoma),  YAPC (Pancreatic carcinoma)')
+    parser.add_argument("--inst", type=str, default='GSE70138_Broad_LINCS_inst_info_2017-03-06.txt.gz', help="Path to inst_info file (.txt.gz)")
+    parser.add_argument("--gctx", type=str, default='GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx', help="Path to LINCS L1000 level 3 GEPs (.gctx)")
+    parser.add_argument("--gene", type=str, default='GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz', help="Path to gene_info file (.txt.gz)")
+    parser.add_argument("--auto", type=bool, default=False, help="Whether to automatically determine the reference cell type, default=False.")
+    parser.add_argument("--gep", type=str, help="Path to GEP.txt for cell type determination.")
 
-    parser.add_argument("-o", "--outdir", default='./LINCS/', help="path to output directory, default='./'")
-    parser.add_argument("-c", "--celltype", default=None, required=True, help='Cell Line name. Options:  A375 (malignant melanoma),  A549 (non-small cell lung carcinoma),  HCC515 (non-small cell lung adenocarcinoma),  HEPG2 (hepatocellular carcinoma), HT29 (colorectal adenocarcinoma),  MCF7 (breast adenocarcinoma),  PC3 (prostate adenocarcinoma),  YAPC (Pancreatic carcinoma)')
-    parser.add_argument("--inst", type=str, default='GSE70138_Broad_LINCS_inst_info_2017-03-06.txt.gz', help="inst_info file (.txt.gz)")
-    parser.add_argument("--gctx", type=str, default='GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx', help="LINCS L1000 level 3 GEPs (.gctx)")
-    parser.add_argument("--gene", type=str, default='GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz', help="gene_info file (.txt.gz)")
-    
     args = parser.parse_args()
 
     cell_types = ['A375','A549','HCC515','HEPG2','HT29','MCF7','PC3','YAPC']
+    do_detect = args.auto
 
-    if not args.celltype in cell_types:
+    if args.celltype and (not args.celltype in cell_types):
         sys.exit('Unacceptable cell type: {}\nSee \'--help\' for acceptable cell line names. '.format(args.celltype))
     if not os.path.isdir(args.outdir):
         os.mkdir(args.outdir)
@@ -44,7 +46,6 @@ if __name__ == '__main__':
     if not (os.path.isfile(args.gctx) and args.gctx.endswith('.gctx')):
         if args.gctx == 'GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx':
             downloadFromGEO('GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx.gz', 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70138/suppl/GSE70138%5FBroad%5FLINCS%5FLevel3%5FINF%5Fmlr12k%5Fn345976x12328%5F2017%2D03%2D06%2Egctx%2Egz')
-            
         else:
             sys.exit("The gctx file does not exist or is not gz file.")
     if not (os.path.isfile(args.gene) and args.gene.endswith('.gz')):
@@ -52,14 +53,33 @@ if __name__ == '__main__':
             downloadFromGEO('GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz', 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70138/suppl/GSE70138%5FBroad%5FLINCS%5Fgene%5Finfo%5F2017%2D03%2D06%2Etxt%2Egz')
         else:
             sys.exit("The gene_info file does not exist or is not .gz file.")
-
+    if do_detect and not os.path.isfile(args.gep):
+        sys.exit("The single-cell gene expression profile ('GEP.txt') for cell type determination cannot be found. Check the file or set --auto to False to manually select the cell type.")
 
     # read inst_info and gene_info
     inst_info = pd.read_csv(args.inst, sep='\t', compression='gzip')
     sig_info = pd.read_csv(args.gene, sep='\t', usecols=['pr_gene_id','pr_gene_symbol'], compression='gzip')
 
     cell = args.celltype
-    
+
+    if do_detect:
+        from scipy.stats import pearsonr
+
+        average_gep = pd.read_csv(args.gep, delimiter='\t', indexl_col=0).mean(axis=1).to_numpy()
+        # normalize to TPM
+        average_gep = average_gep / average_gep.sum() * 1e6
+        cellline_gep = pd.read_csv('./LINCS/CCLE_GEP.csv', index_col=0)
+        # calculate Pearson's correlation between average scGEP and the bulk GEP for each cell line
+        mutual_genes = [x for x in average_gep.index if x in cellline_gep.index]
+        max_p = np.NINF
+        for c in cellline_gep.columns:
+            if(p:=(pearsonr(average_gep[mutual_genes], cellline_gep.loc[mutual_genes, c].to_numpy())) > max_p):
+                max_p = p
+                cell = c
+        print('selected cell type = {}, with Pearsonr = {}', cell, max_p)
+
+
+
     # select instance ids for a specific cell type
     inst_ids = inst_info['inst_id'][inst_info['cell_id'] == cell]
     # read gctx
