66b21afa9120d28af6fea526b50908903daf8676
blackjack061524 <blackjack061524@gmail.com>
2021-08-27 12:28:43 +0800

    fix for CIBERSORTx input directory

--- script/CIBERSORTx_fractions.py
+++ script/CIBERSORTx_fractions.py
@@ -5,20 +5,21 @@ import numpy as np
 ## Parse command-line arguments
 parser = argparse.ArgumentParser(description="impute the fractions of previous identified cell subsets under each bulk sample in the LINCS L1000 database.")
 
-parser.add_argument("-i", "--input", required=True, help="path to input directory")
+parser.add_argument("-i", "--input", required=True, help="path to input single-cell GEP file")
 parser.add_argument("-o", "--output", default='./', help="path to output directory, default='./'")
 parser.add_argument("-u", "--username", required=True, help="email address registered on CIBERSORTx website")
 parser.add_argument("-t", "--token", required=True, help="token obtained from CIBERSORTx website")
-parser.add_argument("-r", "--refsample", required=True, default=None, help="absolute path to the single-cell sample file")
 parser.add_argument("--celltype", default=None, help="choose a cell line from the options. If no name is provided, we will automatically determine the cell type. Options:  A375 (malignant melanoma),  A549 (non-small cell lung carcinoma),  HCC515 (non-small cell lung adenocarcinoma),  HEPG2 (hepatocellular carcinoma), HT29 (colorectal adenocarcinoma),  MCF7 (breast adenocarcinoma),  PC3 (prostate adenocarcinoma),  YAPC (Pancreatic carcinoma)")
 
 args = parser.parse_args()
+
+data_path = '/single-cell-analysis/data/'
 
 function = "docker run --rm --name cibersortx-fractions \
           -v {input_dir}:/src/data -v {output_dir}:/src/outdir \
           cibersortx/fractions --username {username} --token {token} \
           --single_cell TRUE --fraction 0 --rmbatchSmode TRUE ".format(
-          input_dir=os.path.abspath(args.input), output_dir=os.path.abspath(args.output), 
+          input_dir=data_path, output_dir=os.path.abspath(args.output), 
           username=args.username, token=args.token)
 
 ## Check arguments
@@ -27,8 +28,6 @@ if not os.path.exists(args.input):
     sys.exit("The input path does not exist.")
 if not os.path.exists(args.output):
     os.makedirs(args.output)
-if not args.refsample or not (args.refsample and os.path.isfile(args.refsample)):
-    sys.exit("The single-cell sample file does not exist.")
 if not args.celltype:
     # automatically determine the cell line
     from scipy.stats import pearsonr
@@ -45,8 +44,8 @@ if not args.celltype:
         return set(deg_list)
 
     print("determing the cell line...")
-    average_gep = np.log2(pd.read_csv(args.refsample, delimiter='\t', index_col=0, header=0).mean(axis=1)+1)
-    cellline_gep = pd.read_csv('/single-cell-analysis/data/bk_2021_gep.csv', sep=',', index_col=0)
+    average_gep = np.log2(pd.read_csv(args.input, delimiter='\t', index_col=0, header=0).mean(axis=1)+1)
+    cellline_gep = pd.read_csv(data_path + 'bk_2021_gep.csv', sep=',', index_col=0)
     mutual_genes = [x for x in average_gep.index if x in cellline_gep.index]
     mutual_genes = find_deg(cellline_gep.loc[mutual_genes,:])
 
@@ -56,12 +55,12 @@ if not args.celltype:
             max_p = p
             cell = c
     print("selected cell type = {}".format(cell))
-    bulk_path = '/single-cell-analysis/data/LINCS_L1000_GEP_{}.txt'.format(cell)
+    bulk_path = data_path + 'LINCS_L1000_GEP_{}.txt'.format(cell)
 else:
     if not args.celltype in ['A375','A549','HCC515','HEPG2','HT29','MCF7','PC3','YAPC']:
         sys.exit("Unacceptable cell type.")
     cell = args.celltype
-    bulk_path = '/single-cell-analysis/data/LINCS_L1000_GEP_{}.txt'.format(args.celltype)
+    bulk_path = data_path + 'LINCS_L1000_GEP_{}.txt'.format(args.celltype)
 
 if not os.path.isfile(bulk_path):
     from cmapPy.pandasGEXpress.parse import parse
@@ -82,9 +81,9 @@ if not os.path.isfile(bulk_path):
                 with gzip.open(filename, 'rb') as f_in:
                     with open(filename.rsplit('.',1)[0], 'wb') as f_out:
                         shutil.copyfileobj(f_in, f_out)
-    file_inst = '/single-cell-analysis/data/GSE70138_Broad_LINCS_inst_info_2017-03-06.txt.gz'
-    file_sig = '/single-cell-analysis/data/GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz'
-    file_gctx = '/single-cell-analysis/data/GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx.gz'
+    file_inst = data_path + 'GSE70138_Broad_LINCS_inst_info_2017-03-06.txt.gz'
+    file_sig = data_path + 'GSE70138_Broad_LINCS_gene_info_2017-03-06.txt.gz'
+    file_gctx = data_path + 'GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx.gz'
     downloadFromGEO(file_inst, 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70138/suppl/GSE70138%5FBroad%5FLINCS%5Finst%5Finfo%5F2017%2D03%2D06%2Etxt%2Egz')
     downloadFromGEO(file_sig, 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70138/suppl/GSE70138%5FBroad%5FLINCS%5Fgene%5Finfo%5F2017%2D03%2D06%2Etxt%2Egz')
     downloadFromGEO(file_gctx, 'https://ftp.ncbi.nlm.nih.gov/geo/series/GSE70nnn/GSE70138/suppl/GSE70138%5FBroad%5FLINCS%5FLevel3%5FINF%5Fmlr12k%5Fn345976x12328%5F2017%2D03%2D06%2Egctx%2Egz')
@@ -105,8 +104,12 @@ if not os.path.isfile(bulk_path):
     exp_df = 2**max_df
     exp_df.to_csv(bulk_path, sep='\t')
 
-function += "--refsample {} --mixture {}".format(args.refsample, bulk_path)
+refsample = os.path.basename(args.input)
+mixture = os.path.basename(bulk_path)
+function += "--refsample {} --mixture {}".format(refsample, mixture)
 
 
 ## Run CIBERSORTx fractions
+os.system("mv {} {}".format(args.input, data_path + refsample))
 os.system(function)
+os.system("mv {} {}".format(data_path + refsample, args.input))
