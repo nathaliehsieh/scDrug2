fc7a7cb154f3c8c01d6eefa5e6057312f6a45f44
blackjack061524 <blackjack061524@gmail.com>
2021-09-01 09:59:56 +0800

    update draw effect

--- script/draw_effect.py
+++ script/draw_effect.py
@@ -1,4 +1,4 @@
-import argparse, os, sys
+import argparse, os, sys, re
 import pickle
 import pandas as pd
 import seaborn as sns
@@ -32,7 +32,7 @@ def draw_heatmap(df, drugs):
         c_bar = ax.collections[0].colorbar
         c_bar.set_ticks([threshold, conthreshold, 0])
         c_bar.set_ticklabels([str(threshold), str(conthreshold), '\u2265 0'])
-        plt.savefig('selected_drug_heatmap.png',bbox_inches='tight')
+        plt.savefig(os.path.join(args.output, 'selected_drug_heatmap.png'),bbox_inches='tight')
         print('Heatmap : selected_drug_heatmap.png')
 
 
@@ -56,58 +56,62 @@ def draw_consistency_plot(index_list, df_effect, df_all):
     return df_all
     
 
-parser = argparse.ArgumentParser(description='Generate drug effect plots and dataframe(.csv)')
-parser.add_argument('-d', '--dir', required=True, help='the directory where df_effect.pickle and DICT_DRUG_PRE.pickle are stored.')
-parser.add_argument('-t', '--table', required=True, help='path to the solution table (file name: \'*_solution_list_t*_cont*.csv\') generated after running treatment_selection.py.')
+parser = argparse.ArgumentParser(description='Generate drug effect plots and dataframe (.csv)')
+parser.add_argument('-i', '--input', required=True, help='the directory where the files generated after running treatment_selection.py are stored, \
+                    including \'df_effect.pickle\', \'DICT_DRUG_PRE.pickle\' and the solution table (\'*_solution_list_t*_cont*.csv\').')
+parser.add_argument("-o", "--output", default='./', help="path to output directory, default='./'")
 parser.add_argument('--threshold', default=None, help='threshold of cell survival rate used for treatment selection; If not provided, the value wll be set based on the name of the table file.')
 parser.add_argument('--conthreshold', default=None, help='consistency threshold used for treatment selection; If not provided, the value wll be set based on the name of the table file.')
 parser.add_argument('--names', default=None, help='comma-delimited names of drugs for heatmap visualization. Example: palbociclib,NVP-BEZ235,selumetinib')
 
 args = parser.parse_args()
 
-if not os.path.isdir(args.dir):
+
+if not os.path.isdir(args.input):
     sys.exit('The directory does not exist.')
-if not os.path.isfile(args.table):
-    sys.exit('The solution table( .csv) does not exist.')
 
 df_effect = pd.DataFrame()
 DICT_DRUG_PRE = {}
 threshold = conthreshold = 0
+table = None
 
-for file in os.listdir(args.dir):
+for file in os.listdir(args.input):
     if file.endswith('df_effect.pickle'):
-        with open('{}/{}'.format(args.dir, file), 'rb') as fh:
+        with open('{}/{}'.format(args.input, file), 'rb') as fh:
             df_effect = pickle.load(fh)
             df_effect = df_effect.iloc[:, :int(len(df_effect.columns)/2)]
             df_effect = df_effect.reindex(sorted(df_effect.columns, key=int), axis=1)
     elif file.endswith('DICT_DRUG_PRE.pickle'):
-        with open('{}/{}'.format(args.dir, file), 'rb') as fh:
+        with open('{}/{}'.format(args.input, file), 'rb') as fh:
             DRUG_LIST_PRE = pickle.load(fh)
+    elif re.match(".*_solution_list_t.*_cont.*\.csv", file):
+        table = os.path.join(args.input, file)
 
 if df_effect.size == 0:
     sys.exit('df_effect.pickle does not exist or is empty.')
 if len(DRUG_LIST_PRE) == 0:
     sys.exit('DRUG_LIST_PRE.pickle does not exist or is empty.')
+if not table:
+    sys.exit('The solution table (.csv) does not exist.')
 
 if args.threshold and args.threshold <= 1 and args.threshold >= -1 :
     threshold = args.threshold
 else:
-    threshold = float(args.table.rsplit('_',2)[1].rsplit('t',1)[1])
+    threshold = float(table.rsplit('_',2)[1].rsplit('t',1)[1])
 if args.conthreshold and args.conthreshold <= 1 and args.conthreshold >= -1 :
     conthreshold = args.conthreshold
 else:
-    conthreshold = float(args.table.rsplit('.',1)[0].rsplit('cont',1)[1])
+    conthreshold = float(table.rsplit('.',1)[0].rsplit('cont',1)[1])
 
 
-
-keywords = list(set(pd.read_csv(args.table, header=None).values.flatten()))
+keywords = list(set(pd.read_csv(table, header=None).values.flatten()))
 
 keywords = list(set([x.split('_',1)[0] for x in keywords if isinstance(x, str) ]))
 print('{} unique drugs.'.format(len(keywords)))
 
 DICT_INDEX = {}
 
-pdfname = 'treatment_effect.pdf'
+pdfname = os.path.join(args.output, 'treatment_effect.pdf')
 pp = PdfPages(pdfname)
 df_all = pd.DataFrame(columns = df_effect.columns)
 for drug_id in sorted(keywords):
@@ -115,8 +119,10 @@ for drug_id in sorted(keywords):
     df_all = draw_consistency_plot(DICT_INDEX[drug_id], df_effect, df_all)
 pp.close()
 
-df_all.to_csv('treatment_effect.csv', index=True)
-print('Figures are stored in \'{}\' . \n Values are stored in \'treatment_effect.csv\' .'.format(pdfname))
+csvname = os.path.join(args.output, 'treatment_effect.csv')
+df_all.to_csv(csvname, index=True)
+print('Figures are stored in \'{}\'.'.format(pdfname))
+print('Values are stored in \'{}\'.'.format(csvname))
 
 if args.names:
     draw_heatmap(df_all, args.names.split(','))
